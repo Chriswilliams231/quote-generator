f78f57faf085f3be17914fde09760e7a550b36b6
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2023-02-02 18:15:48 -0800

    trying to genorate a new quote with the api

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+let apiQuotes = [];
+
+// show new quote 
+const newQuote = () => {
+
+}
 // Getting quotes from a github api
 const getQuotes = async () => {
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
     try {
         const response = await fetch(apiUrl);
+        apiQuotes = response.json();
+        newQuote()
     } catch (err) {
         console.log(err)
     }
 }
+
+// On Load
+getQuotes();
