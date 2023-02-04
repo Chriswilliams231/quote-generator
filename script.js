9685bde4f0478ef337311b857395ac370d97d9df
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2023-02-04 10:51:57 -0800

    using local quotes for now

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-let apiQuotes = [];
+// let apiQuotes = [];
 
 // show new quote 
 const newQuote = () => {
-
+    // Picks a random quote from the apiQuotes array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+    console.log(quote)
 }
 // Getting quotes from a github api
-const getQuotes = async () => {
-    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+// const getQuotes = async () => {
+//     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = response.json();
-        newQuote()
-    } catch (err) {
-        console.log(err)
-    }
-}
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = response.json();
+//         console.log(apiQuotes)
+//         newQuote()
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
 
 // On Load
-getQuotes();
+// getQuotes();
+newQuote()
